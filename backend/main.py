02d8f4454a2dfcc22ce633ce5530eb9f4a930643
KouheiWatanabe-CS <k.watanabe@cloudsmith.co.jp>
2022-12-06 17:48:46 +0900

    プロキシ設定追加

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 
 app = FastAPI()
 
 
 @app.get("/api")
-async def root():
+async def root(request:Request):
+    print(request.headers)
     return {"message": "Hello World"}
+
+@app.get("/no-proxy-header")
+async def noProxyHeader(request:Request):
+    print(request.headers)
+    return {"message": "no proxy header"}
